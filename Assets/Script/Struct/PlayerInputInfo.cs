483cf289ccf448a2e5f108ce3dc884bea11ca898
Scream <scream870102@gmail.com>
2019-02-23 13:42:20 +0800

    修改playerInputInfo

--- Assets/Script/Struct/PlayerInputInfo.cs
+++ Assets/Script/Struct/PlayerInputInfo.cs
@@ -3,4 +3,8 @@ public struct PlayerInputInfo
 {
     public EColor color;
     public float time;
+    public PlayerInputInfo(EColor color,float time){
+        this.color=color;
+        this.time=time;
+    }
 }
