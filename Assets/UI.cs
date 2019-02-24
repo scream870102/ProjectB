000a560b16e89d9eac473f780877e386c2e03930
Scream <scream870102@gmail.com>
2019-02-24 17:47:43 +0800

    temp

--- Assets/UI.cs
+++ Assets/UI.cs
@@ -1,19 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class UI : MonoBehaviour
-{
-    public GameObject 
-    // Start is called before the first frame update
-    void Start()
-    {
+// public class UI : MonoBehaviour
+// {
+//     public GameObject 
+//     // Start is called before the first frame update
+//     // void Start()
+//     // {
         
-    }
+//     // }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //GameManager.instance.player1.InputResults;
-    }
-}
+//     // Update is called once per frame
+//     // void Update()
+//     // {
+//     //     //GameManager.instance.player1.InputResults;
+//     // }
+// }
